f2df3dc0c8cd7899839f6c06896fcc5654244e30
Rick <richims026@gmail.com>
2025-03-20 09:21:24 -0500

    Cambios 2

--- main.R
+++ main.R
@@ -1,1 +1,42 @@
 dir()
+# Hay 3 maneras de representarlos: Date, POSIXct, POSIXlt
+Sys.Date() %>% class()
+Sys.time() %>% class()
+
+# %Y: Año con 4 dígitos.
+# %y: Año con 2 dígitos.
+# %m: Mes (01-12).
+# %d: Día (01-31).
+# %B: Nombre completo del mes.
+# %b: Nombre abreviado del mes.
+
+# Date (Solo fecha)
+myfec <- as.Date("2025/04/02")
+myfec <- as.Date("2025-04-02")
+
+myfec <- as.Date("2025/04/02", format = "%Y/%m/%d")
+myfec <- as.Date("02_05_2025", format = "%d_%m_%Y")
+myfec <- as.Date("02_Jan_2025", format = "%d_%b_%Y")
+
+myfec + 7
+
+# %H: Hora.
+# %M: Minuto.
+# %S: Segundo.
+
+# POSIXct (Fecha y Hora)
+myfecp <- as.POSIXct("2025-04_03 10:30:25", format = "%Y-%m_%d %H:%M:%S")
+myfecp + 1
+
+# Formatear Fecha
+format(myfecp,"%d de %B, %y -> %H:%M::%S")
+
+# Operaciones con fechas
+library(lubridate)
+year(myfec)
+month(myfec)
+day(myfec)
+hour(myfec)
+minute(myfec)
+second(myfec)
+
