55a40e654417b498ded1764f1816e7fe386090a4
Rick <richims026@gmail.com>
2025-03-20 12:51:25 -0500

    Cambios 3

--- main.R
+++ main.R
@@ -12,31 +12,52 @@ Sys.time() %>% class()
 
 # Date (Solo fecha)
 myfec <- as.Date("2025/04/02")
-myfec <- as.Date("2025-04-02")
+myfec <- as.Date("2025-04-03")
 
 myfec <- as.Date("2025/04/02", format = "%Y/%m/%d")
 myfec <- as.Date("02_05_2025", format = "%d_%m_%Y")
 myfec <- as.Date("02_Jan_2025", format = "%d_%b_%Y")
 
-myfec + 7
-
+myfec + 7 - myfec
 # %H: Hora.
 # %M: Minuto.
 # %S: Segundo.
 
 # POSIXct (Fecha y Hora)
 myfecp <- as.POSIXct("2025-04_03 10:30:25", format = "%Y-%m_%d %H:%M:%S")
-myfecp + 1
+
+myfecp + (3600 * 24) - myfecp
+
+# Zona horaria
+myfecp <- as.POSIXct("2025-04_03 10:30:25",
+  format = "%Y-%m_%d %H:%M:%S",
+  tz = "America/Lima"
+)
+
+# POSIXlt (Fecha y Hora)
+myfecp <- as.POSIXlt("2025-04_03 10:30:25",
+  format = "%Y-%m_%d %H:%M:%S",
+  tz = "America/Lima"
+)
+
+myfecp %>% unlist
+myfecp$mday
+
+OlsonNames() # Listar Zonas Horarias
+Sys.timezone() # Zona Hoaria del sistema
 
 # Formatear Fecha
-format(myfecp,"%d de %B, %y -> %H:%M::%S")
+format(myfecp, "%d de %B, %y -> %H:%M::%S")
+format(Sys.time(), "%Y-%m-%d_%H:%M::%S")
 
 # Operaciones con fechas
 library(lubridate)
 year(myfec)
-month(myfec)
+month(myfec, label = TRUE)
 day(myfec)
 hour(myfec)
 minute(myfec)
 second(myfec)
 
+myfec + days(5)
+myfecp + days(5)
